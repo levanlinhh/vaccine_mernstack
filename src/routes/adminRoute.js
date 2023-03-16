4474b2d5283a100ed2b5407a85466ed607a09709
LÊ VĂN LĨNH <104518175+levanlinhh@users.noreply.github.com>
2023-03-16 10:48:15 +0700

    Hoàn thành call api summary data

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
-const adminController = require('../controllers/adminController');
+const { adminController } = require('../controllers');
+const tokenHandler = require('../handlers/tokenHandler');
 
 
 router.post('/login', adminController.login);
+
+router.get(
+    '/summary',
+    tokenHandler.verifyAdminToken,
+    adminController.summary
+)
+
+router.post(
+    '/check-token',
+    tokenHandler.verifyAdminToken,
+    (req, res) => {
+        res.status(200).json('Authorized');
+    }
+)
+
 
 module.exports = router;
