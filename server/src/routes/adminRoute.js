02c6542932f704d5fc18be53092c17b478d24af3
LÊ VĂN LĨNH <104518175+levanlinhh@users.noreply.github.com>
2023-03-27 20:56:30 +0700

    hoàn thành đồ án

--- server/src/routes/adminRoute.js
+++ server/src/routes/adminRoute.js
@@ -1,23 +1,13 @@
-const router = require('express').Router();
-const { adminController } = require('../controllers');
-const tokenHandler = require('../handlers/tokenHandler');
+const router = require("express").Router();
+const { adminController } = require("../controllers");
+const tokenHandler = require("../handlers/tokenHandler");
 
+router.post("/login", adminController.login);
 
-router.post('/login', adminController.login);
+router.get("/summary", tokenHandler.verifyAdminToken, adminController.summary);
 
-router.get(
-    '/summary',
-    tokenHandler.verifyAdminToken,
-    adminController.summary
-)
-
-router.post(
-    '/check-token',
-    tokenHandler.verifyAdminToken,
-    (req, res) => {
-        res.status(200).json('Authorized');
-    }
-)
-
+router.post("/check-token", tokenHandler.verifyAdminToken, (req, res) => {
+  res.status(200).json("Authorized");
+});
 
 module.exports = router;
